65f2ca0bbad2badbc8c3bb296c9b9fe67559e12f
Omri Wallach <omriwallach1992@gmail.com>
2025-12-08 20:32:40 +0100

    refactor: indent

--- scripts/deploy_remote.py
+++ scripts/deploy_remote.py
@@ -76,7 +76,7 @@ def main():
         f"git reset --hard origin/{BRANCH}"
     )
 
-     print("Installing dependencies")
+    print("Installing dependencies")
     run_remote(
         ssh,
         "kubectl apply -f https://raw.githubusercontent.com/traefik/traefik/v2.10/docs/content/reference/dynamic-configuration/kubernetes-crd-definition-v1.yml",
