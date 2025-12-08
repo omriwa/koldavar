4ef67dba490745402377a857d99f0e80f0e4a874
Omri Wallach <omriwallach1992@gmail.com>
2025-12-08 20:58:48 +0100

    refactor: helm chart upgrade

--- scripts/deploy_remote.py
+++ scripts/deploy_remote.py
@@ -80,7 +80,7 @@ def main():
     print("Applying Kubernetes manifests with helm...")
     run_remote(
         ssh,
-        "cd koldavar/k8s/helm/koldavar-chart && helm dependency upgrade && helm upgrade koldavar --install . -f values.yaml",
+        "cd koldavar/k8s/helm/koldavar-chart && helm dependency upgrade && helm upgrade koldavar . --install",
         fail_on_error=True
     )
 
